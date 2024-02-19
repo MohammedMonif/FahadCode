65b9cfed7578a655065312e99bedcdd7b210bb4e
Fahad-Saad <148910681+Fahad-Saad@users.noreply.github.com>
2024-02-19 10:23:40 +0000

    Update mfk2.h

--- mfk2.h
+++ mfk2.h
@@ -2,7 +2,7 @@
 #ifndef MFK_H
 #define MFK_H
 
-char* blake2(const char* data); // Updated function name
+char* shake128(const char* data); // Renamed to reflect using SHAKE128 
 char* check(const char* arg1, int arg2);
 
 #endif // MFK_H
